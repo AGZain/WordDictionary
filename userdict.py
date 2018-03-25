a032eefb562ef2cff7cb998a4a8bbb900215955d
mahumk <31860963+mahumk@users.noreply.github.com>
2018-03-24 23:37:00 -0400

    Add files via upload

---
+++ userdict.py
@@ -0,0 +1,14 @@
+from dictionaryDt import dictionary
+import sys
+
+theDict = dictionary("theDict")
+
+print("Welcome to the dictionary!")
+print('Enter "n" to enter new word/definition')
+print('Enter "e" to edit a definition')
+print ('Enter "s" to search for a word')
+response = input("Please enter a letter ")
+print(response)
+
+
+
