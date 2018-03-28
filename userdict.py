8120eab60605297bf7044a18257b1ecb8377a6eb
mahumk <31860963+mahumk@users.noreply.github.com>
2018-03-28 16:18:58 -0400

    Update userdict.py

--- userdict.py
+++ userdict.py
@@ -10,9 +10,17 @@ response = input("Please enter a letter ")
 print(response)
 
 if response=="n":
-
+  word = input("Please enter a word")
+  definition = input("Please enter a definition")
+	theDict.put(word,definition)
+    
 else if response=="e":
+  word= input("Enter word to change definition")
+  definition= input("Enter new definition")
+  theDict.update(word,definition)
 
 else if response=="s":
+  word=input("Enter word to search")
+  theDict.get(word)
   
   
