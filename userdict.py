81129de0b4a7001d3268f2600a89ee8dfa46955f
mahumk <31860963+mahumk@users.noreply.github.com>
2018-03-28 16:31:07 -0400

    Update userdict.py

--- userdict.py
+++ userdict.py
@@ -2,25 +2,29 @@ from dictionaryDt import dictionary
 
 theDict = dictionary("theDict")
 
-print("Welcome to the dictionary!")
-print('Enter "n" to enter new word/definition')
-print('Enter "e" to edit a definition')
-print ('Enter "s" to search for a word')
-response = input("Please enter a letter ")
-print(response)
+var = 1
+while var == 1 :
 
-if response=="n":
-  word = input("Please enter a word")
-  definition = input("Please enter a definition")
-	theDict.put(word,definition)
-    
-else if response=="e":
-  word= input("Enter word to change definition")
-  definition= input("Enter new definition")
-  theDict.update(word,definition)
+    print("Welcome to the dictionary!")
+    print('Enter "n" to enter new word/definition ')
+    print('Enter "e" to edit a definition ')
+    print ('Enter "s" to search for a word ')
+    response = input("Please enter a letter ")
+    print(response)
 
-else if response=="s":
-  word=input("Enter word to search")
-  theDict.get(word)
-  
-  
+    if response=="n":
+      word = input("Please enter a word")
+      definition = input("Please enter a definition")
+      theDict.put(word,definition)
+        
+    elif response=="e":
+      word= input("Enter word to change definition")
+      definition= input("Enter new definition")
+      theDict.update(word,definition)
+
+    elif response=="s":
+      word=input("Enter word to search")
+      result=theDict.get(word)
+      for i in range(0,len(result)):
+          print(result[i]);
+
